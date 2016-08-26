5672a3639e9410e0b65c9aa571912b992ed20a2b
philadelphia <tao.zt.zhang@deltaww.com>
2016-08-26 15:34:07 +0800

    add a new feature about deleting an item from recycleview by using ContextMenu

--- myapplication/src/main/java/com/example/myapplication/ui/manager/PackageManagerFragment.java
+++ myapplication/src/main/java/com/example/myapplication/ui/manager/PackageManagerFragment.java
@@ -1,27 +1,28 @@
 package com.example.myapplication.ui.manager;
 
 
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
-import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
 import com.example.myapplication.adapter.RecyclerViewAdapter;
 import com.example.myapplication.utils.CustomItemClickListener;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -31,14 +32,16 @@ import butterknife.OnClick;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PackageManagerFragment extends Fragment implements CustomItemClickListener{
+public class PackageManagerFragment extends Fragment implements CustomItemClickListener {
     private PackageManager packageManager;
     private List<PackageInfo> installedPackages;
-    private List<String > pkgNameList;
-    private RecyclerViewAdapter myAdapter ;
+    private List<String> pkgNameList;
+    private RecyclerViewAdapter myAdapter;
     @BindView(R.id.recyclerView)
     RecyclerView recyclerView;
-//    private  RecyclerView recyclerView;
+    //    private  RecyclerView recyclerView;
+
+
     private static final String TAG = "PackageManagerFragment";
 
     @Override
@@ -53,14 +56,18 @@ public class PackageManagerFragment extends Fragment implements CustomItemClickL
         initData();
         myAdapter = new RecyclerViewAdapter(installedPackages);
         myAdapter.setOnCustomeItemClickListener(this);
+
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),
                 LinearLayout.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
+
+        registerForContextMenu(recyclerView);
         recyclerView.setAdapter(myAdapter);
         return view;
 
     }
-    public void initData(){
+
+    public void initData() {
         Log.i(TAG, "initData: ");
         installedPackages = packageManager.getInstalledPackages(0);
         Log.i(TAG, "installedPackages: " + installedPackages.size());
@@ -80,6 +87,75 @@ public class PackageManagerFragment extends Fragment implements CustomItemClickL
 
     @Override
     public void onItemClick(View v, int position) {
-        Snackbar.make(recyclerView,"ddd",Snackbar.LENGTH_SHORT).show();
+        Snackbar.make(recyclerView, "ddd", Snackbar.LENGTH_SHORT).show();
+//        Log.i(TAG, "onItemClick: " + position);
+//        PopupMenu menu = new PopupMenu(getContext(), v,Gravity.CENTER);
+//        menu.inflate(R.menu.popup_menu);
+//        menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+//            @Override
+//            public boolean onMenuItemClick(MenuItem item) {
+//                return true;
+//            }
+//        });
+////
+////
+//////        menu.setGravity(Gravity.CENTER);
+//        menu.show();
+
+
+    }
+
+    @Override
+    public void onItemLongClick(View v, int position) {
+
+    }
+
+    @Override
+    public void registerForContextMenu(View view) {
+        Log.i(TAG, "registerForContextMenu: ");
+        super.registerForContextMenu(view);
+    }
+
+    @Override
+    public void unregisterForContextMenu(View view) {
+        super.unregisterForContextMenu(view);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item == null) {
+            Log.i(TAG, "onContextItemSelected: item == null");
+        }
+        int position = -1;
+        position = myAdapter.getPosition();
+        Log.i(TAG, "position == :"+ position);
+        AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        if (menuInfo == null) {
+            Log.i(TAG, "menuInfo: null");
+
+        }
+        switch (item.getItemId()) {
+            case 0:
+                signAsImportant();
+
+                break;
+            case 1:
+                deleteItem(position);
+                Log.i(TAG, "deleting......: ");
+                break;
+            default:
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    private void deleteItem(int position) {
+        Log.i(TAG, "deleteItem: " + position);
+        installedPackages.remove(position);
+        myAdapter.notifyDataSetChanged();
+    }
+
+
+    private void signAsImportant() {
     }
 }
--- myapplication/src/main/java/com/example/myapplication/utils/CustomItemClickListener.java
+++ myapplication/src/main/java/com/example/myapplication/utils/CustomItemClickListener.java
@@ -7,4 +7,5 @@ import android.view.View;
  */
 public interface CustomItemClickListener {
     public void onItemClick(View v, int position);
+    public void onItemLongClick(View v, int position);
 }
