b99862def25f5f168a12d1de1d20bc4b1d190ffb
tao.zt.zhang <zt%123456>
2017-08-29 16:48:06 +0800

    封装网络请求框架。可以无痛的在不同的HttpClient之间无缝切换

---
+++ myapplication/src/main/java/com/example/myapplication/entity/Result.java
@@ -0,0 +1,369 @@
+package com.example.myapplication.entity;
+
+import java.util.List;
+
+/**
+ * Created by Tao.ZT.Zhang on 2017/8/29.
+ */
+
+public class Result {
+
+
+    /**
+     * postid : PHOT24LDS000100A
+     * series :
+     * clientadurl :
+     * desc : 4月14日早上7点10分左右，巡特警二大队接到报警电话称芙蓉溪仙人桥段有一浮尸。民警赶到现场后发现，河里有一名女性浮尸，岸边还有疑似死者脱下的衣服、鞋子。
+     * datatime : 2017-04-16 15:29:49
+     * createdate : 2017-04-16 14:21:05
+     * relatedids : []
+     * scover : http://img3.cache.netease.com/photo/0001/2017-04-16/s_CI5AUVQ500AP0001.jpg
+     * autoid :
+     * url : http://news.163.com/photoview/00AP0001/2250172.html
+     * creator : 耿旭娜
+     * reporter :
+     * photos : [{"timgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/t_CI5AUVQ000AP0001.jpg","photohtml":"http://news.163.com/photoview/00AP0001/2250172.html#p=CI5AUVQ000AP0001","newsurl":"#","squareimgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/400x400_CI5AUVQ000AP0001.jpg","cimgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/c_CI5AUVQ000AP0001.jpg","imgtitle":"","simgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/s_CI5AUVQ000AP0001.jpg","note":"4月14日早上7点10分左右，巡特警二大队接到报警电话称芙蓉溪仙人桥段有一浮尸。民警赶到现场后发现，河里有一名女性浮尸，岸边还有疑似死者脱下的衣服、鞋子。（来源：直播绵阳）","photoid":"CI5AUVQ000AP0001","imgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/CI5AUVQ000AP0001.jpg"},{"timgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/t_CI5AUVQ100AP0001.jpg","photohtml":"http://news.163.com/photoview/00AP0001/2250172.html#p=CI5AUVQ100AP0001","newsurl":"#","squareimgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/400x400_CI5AUVQ100AP0001.jpg","cimgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/c_CI5AUVQ100AP0001.jpg","imgtitle":"","simgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/s_CI5AUVQ100AP0001.jpg","note":"7点30分左右，消防队员将尸体打捞上岸。此时，市民郭某经过此处，本是凑个热闹，却发现死者是自己的爱人胡某。（来源：直播绵阳）","photoid":"CI5AUVQ100AP0001","imgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/CI5AUVQ100AP0001.jpg"},{"timgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/t_CI5AUVQ200AP0001.jpg","photohtml":"http://news.163.com/photoview/00AP0001/2250172.html#p=CI5AUVQ200AP0001","newsurl":"#","squareimgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/400x400_CI5AUVQ200AP0001.jpg","cimgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/c_CI5AUVQ200AP0001.jpg","imgtitle":"","simgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/s_CI5AUVQ200AP0001.jpg","note":"据郭某介绍，今年一月份的时候，自己的独子出了交通事故，不幸死亡。此后，妻子一直未从悲痛中走出，身体状况也不是很好，\u201c我昨天还带她到医院去看病，但她一直拒绝吃药。\u201d郭某说。（来源：直播绵阳）","photoid":"CI5AUVQ200AP0001","imgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/CI5AUVQ200AP0001.jpg"},{"timgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/t_CI5AUVQ300AP0001.jpg","photohtml":"http://news.163.com/photoview/00AP0001/2250172.html#p=CI5AUVQ300AP0001","newsurl":"#","squareimgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/400x400_CI5AUVQ300AP0001.jpg","cimgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/c_CI5AUVQ300AP0001.jpg","imgtitle":"","simgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/s_CI5AUVQ300AP0001.jpg","note":"随后，和胡某一起居住的儿媳打来电话称，在家里发现了一纸遗书，上面嘱咐儿媳带好孙女，并说\u201c我在仙女（人）桥河里 请打110\u201d。目前，游仙镇派出所已介入此事进行调查。（来源：直播绵阳）","photoid":"CI5AUVQ300AP0001","imgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/CI5AUVQ300AP0001.jpg"},{"timgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/t_CI5AUVQ400AP0001.jpg","photohtml":"http://news.163.com/photoview/00AP0001/2250172.html#p=CI5AUVQ400AP0001","newsurl":"#","squareimgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/400x400_CI5AUVQ400AP0001.jpg","cimgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/c_CI5AUVQ400AP0001.jpg","imgtitle":"","simgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/s_CI5AUVQ400AP0001.jpg","note":"图为郭某被警察搀扶着离开。（来源：直播绵阳）","photoid":"CI5AUVQ400AP0001","imgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/CI5AUVQ400AP0001.jpg"},{"timgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/t_CI5AUVQ500AP0001.jpg","photohtml":"http://news.163.com/photoview/00AP0001/2250172.html#p=CI5AUVQ500AP0001","newsurl":"#","squareimgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/400x400_CI5AUVQ500AP0001.jpg","cimgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/c_CI5AUVQ500AP0001.jpg","imgtitle":"","simgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/s_CI5AUVQ500AP0001.jpg","note":"事发现场。（来源：直播绵阳）","photoid":"CI5AUVQ500AP0001","imgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/CI5AUVQ500AP0001.jpg"}]
+     * setname : 男子路遇捞尸凑热闹 发现死者系自己妻子
+     * neteasecode :
+     * cover : http://img3.cache.netease.com/photo/0001/2017-04-16/CI5AUVQ500AP0001.jpg
+     * commenturl : http://comment.news.163.com/photoview_bbs/PHOT24LDS000100A.html
+     * source : 直播绵阳
+     * settag : 男子，凑热闹，妻子
+     * boardid : photoview_bbs
+     * tcover : http://img3.cache.netease.com/photo/0001/2017-04-16/t_CI5AUVQ500AP0001.jpg
+     * imgsum : 6
+     */
+
+    private String postid;
+    private String series;
+    private String clientadurl;
+    private String desc;
+    private String datatime;
+    private String createdate;
+    private String scover;
+    private String autoid;
+    private String url;
+    private String creator;
+    private String reporter;
+    private String setname;
+    private String neteasecode;
+    private String cover;
+    private String commenturl;
+    private String source;
+    private String settag;
+    private String boardid;
+    private String tcover;
+    private String imgsum;
+    private List<?> relatedids;
+    private List<PhotosBean> photos;
+
+    public String getPostid() {
+        return postid;
+    }
+
+    public void setPostid(String postid) {
+        this.postid = postid;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public String getClientadurl() {
+        return clientadurl;
+    }
+
+    public void setClientadurl(String clientadurl) {
+        this.clientadurl = clientadurl;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDatatime() {
+        return datatime;
+    }
+
+    public void setDatatime(String datatime) {
+        this.datatime = datatime;
+    }
+
+    public String getCreatedate() {
+        return createdate;
+    }
+
+    public void setCreatedate(String createdate) {
+        this.createdate = createdate;
+    }
+
+    public String getScover() {
+        return scover;
+    }
+
+    public void setScover(String scover) {
+        this.scover = scover;
+    }
+
+    public String getAutoid() {
+        return autoid;
+    }
+
+    public void setAutoid(String autoid) {
+        this.autoid = autoid;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getReporter() {
+        return reporter;
+    }
+
+    public void setReporter(String reporter) {
+        this.reporter = reporter;
+    }
+
+    public String getSetname() {
+        return setname;
+    }
+
+    public void setSetname(String setname) {
+        this.setname = setname;
+    }
+
+    public String getNeteasecode() {
+        return neteasecode;
+    }
+
+    public void setNeteasecode(String neteasecode) {
+        this.neteasecode = neteasecode;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getCommenturl() {
+        return commenturl;
+    }
+
+    public void setCommenturl(String commenturl) {
+        this.commenturl = commenturl;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getSettag() {
+        return settag;
+    }
+
+    public void setSettag(String settag) {
+        this.settag = settag;
+    }
+
+    public String getBoardid() {
+        return boardid;
+    }
+
+    public void setBoardid(String boardid) {
+        this.boardid = boardid;
+    }
+
+    public String getTcover() {
+        return tcover;
+    }
+
+    public void setTcover(String tcover) {
+        this.tcover = tcover;
+    }
+
+    public String getImgsum() {
+        return imgsum;
+    }
+
+    public void setImgsum(String imgsum) {
+        this.imgsum = imgsum;
+    }
+
+    public List<?> getRelatedids() {
+        return relatedids;
+    }
+
+    public void setRelatedids(List<?> relatedids) {
+        this.relatedids = relatedids;
+    }
+
+    public List<PhotosBean> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<PhotosBean> photos) {
+        this.photos = photos;
+    }
+
+    public static class PhotosBean {
+        /**
+         * timgurl : http://img3.cache.netease.com/photo/0001/2017-04-16/t_CI5AUVQ000AP0001.jpg
+         * photohtml : http://news.163.com/photoview/00AP0001/2250172.html#p=CI5AUVQ000AP0001
+         * newsurl : #
+         * squareimgurl : http://img4.cache.netease.com/photo/0001/2017-04-16/400x400_CI5AUVQ000AP0001.jpg
+         * cimgurl : http://img3.cache.netease.com/photo/0001/2017-04-16/c_CI5AUVQ000AP0001.jpg
+         * imgtitle :
+         * simgurl : http://img4.cache.netease.com/photo/0001/2017-04-16/s_CI5AUVQ000AP0001.jpg
+         * note : 4月14日早上7点10分左右，巡特警二大队接到报警电话称芙蓉溪仙人桥段有一浮尸。民警赶到现场后发现，河里有一名女性浮尸，岸边还有疑似死者脱下的衣服、鞋子。（来源：直播绵阳）
+         * photoid : CI5AUVQ000AP0001
+         * imgurl : http://img3.cache.netease.com/photo/0001/2017-04-16/CI5AUVQ000AP0001.jpg
+         */
+
+        private String timgurl;
+        private String photohtml;
+        private String newsurl;
+        private String squareimgurl;
+        private String cimgurl;
+        private String imgtitle;
+        private String simgurl;
+        private String note;
+        private String photoid;
+        private String imgurl;
+
+        public String getTimgurl() {
+            return timgurl;
+        }
+
+        public void setTimgurl(String timgurl) {
+            this.timgurl = timgurl;
+        }
+
+        public String getPhotohtml() {
+            return photohtml;
+        }
+
+        public void setPhotohtml(String photohtml) {
+            this.photohtml = photohtml;
+        }
+
+        public String getNewsurl() {
+            return newsurl;
+        }
+
+        public void setNewsurl(String newsurl) {
+            this.newsurl = newsurl;
+        }
+
+        public String getSquareimgurl() {
+            return squareimgurl;
+        }
+
+        public void setSquareimgurl(String squareimgurl) {
+            this.squareimgurl = squareimgurl;
+        }
+
+        public String getCimgurl() {
+            return cimgurl;
+        }
+
+        public void setCimgurl(String cimgurl) {
+            this.cimgurl = cimgurl;
+        }
+
+        public String getImgtitle() {
+            return imgtitle;
+        }
+
+        public void setImgtitle(String imgtitle) {
+            this.imgtitle = imgtitle;
+        }
+
+        public String getSimgurl() {
+            return simgurl;
+        }
+
+        public void setSimgurl(String simgurl) {
+            this.simgurl = simgurl;
+        }
+
+        public String getNote() {
+            return note;
+        }
+
+        public void setNote(String note) {
+            this.note = note;
+        }
+
+        public String getPhotoid() {
+            return photoid;
+        }
+
+        public void setPhotoid(String photoid) {
+            this.photoid = photoid;
+        }
+
+        public String getImgurl() {
+            return imgurl;
+        }
+
+        public void setImgurl(String imgurl) {
+            this.imgurl = imgurl;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "postid='" + postid + '\'' +
+                ", series='" + series + '\'' +
+                ", clientadurl='" + clientadurl + '\'' +
+                ", desc='" + desc + '\'' +
+                ", datatime='" + datatime + '\'' +
+                ", createdate='" + createdate + '\'' +
+                ", scover='" + scover + '\'' +
+                ", autoid='" + autoid + '\'' +
+                ", url='" + url + '\'' +
+                ", creator='" + creator + '\'' +
+                ", reporter='" + reporter + '\'' +
+                ", setname='" + setname + '\'' +
+                ", neteasecode='" + neteasecode + '\'' +
+                ", cover='" + cover + '\'' +
+                ", commenturl='" + commenturl + '\'' +
+                ", source='" + source + '\'' +
+                ", settag='" + settag + '\'' +
+                ", boardid='" + boardid + '\'' +
+                ", tcover='" + tcover + '\'' +
+                ", imgsum='" + imgsum + '\'' +
+                ", relatedids=" + relatedids +
+                ", photos=" + photos +
+                '}';
+    }
+}
