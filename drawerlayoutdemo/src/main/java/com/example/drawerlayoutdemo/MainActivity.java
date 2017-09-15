088c0779cf5d721b3d9167932289a9544a0ec763
tao.zt.zhang <zt%123456>
2017-09-15 10:45:39 +0800

    add TextInputLayout相关知识

--- drawerlayoutdemo/src/main/java/com/example/drawerlayoutdemo/MainActivity.java
+++ drawerlayoutdemo/src/main/java/com/example/drawerlayoutdemo/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.drawerlayoutdemo;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity  {
+    private static final String TAG = "MainActivity";
     private DrawerLayout mDrawerLayout;
 
     private Toolbar mToolBar;
@@ -26,6 +32,48 @@ public class MainActivity extends AppCompatActivity  {
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolBar, R.string.open, R.string.close);
         actionBarDrawerToggle.syncState();
         mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        //TextInputLayout
+        TextInputLayout textInputLayout = (TextInputLayout) findViewById(R.id.textInputLayout);
+
+        //开启计数功能
+        textInputLayout.setCounterEnabled(true);
+        textInputLayout.setCounterMaxLength(10);
+//        textInputLayout.getEditText().addTextChangedListener(new MinLengthTextWatcher(textInputLayout, "长度应低于6位数"));
+    }
+
+    static class MinLengthTextWatcher implements TextWatcher{
+        private TextInputLayout textInputLayout;
+        private String errorString;
+
+        public MinLengthTextWatcher( TextInputLayout textInputLayout,  String errorString) {
+            this.textInputLayout = textInputLayout;
+            this.errorString = errorString;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            Log.i(TAG, "beforeTextChanged: ");
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            Log.i(TAG, "onTextChanged: ");
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            Log.i(TAG, "afterTextChanged: ");
+
+            if (textInputLayout.getEditText().getText().toString().trim().length() <= 6){
+                textInputLayout.setErrorEnabled(false);
+//                textInputLayout.getEditText().setFilters((new InputFilter[]{new InputFilter.LengthFilter(6)}));
+            }else {
+                textInputLayout.setErrorEnabled(true);
+                textInputLayout.setError(errorString);
+            }
+
+        }
     }
 
 }
