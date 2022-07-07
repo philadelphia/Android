41c581645cf25b05d24e1d7865cd452f17780016
zhangtao86 <zhangtao86@guazi.com>
2022-07-07 22:32:05 +0800

    upgrade build tool version

---
+++ myapplication/src/main/java/com/example/myapplication/recevier/BootReceiver.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication.recevier
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+/**
+@Author zhang tao
+@Date   7/7/22 9:45 PM
+@Desc
+ */
+private const val TAG = "BootReceiver"
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when (intent?.action) {
+            Intent.ACTION_BOOT_COMPLETED -> Log.e(TAG, "onReceive: boot completed")
+            Intent.ACTION_SHUTDOWN -> Log.e(TAG, "onReceive: shutdown ")
+        }
+    }
+}
